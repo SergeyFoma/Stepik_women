d53a24ba9c812fa55976f73f073550fe18279ef6
SergeyFoma <asdfg2105c@gmail.com>
2025-04-01 19:16:07 +0500

    login logout forms

--- users/form.py
+++ users/form.py
@@ -1,2 +1,7 @@
 from django import forms
 
+class LoginUserForm(forms.Form):
+    username=forms.CharField(label="login", widget=forms.TextInput(attrs={"class":"form-input"}))
+    password=forms.CharField(label='password', widget=forms.PasswordInput(attrs={"class":"form-input"}))
+
+
