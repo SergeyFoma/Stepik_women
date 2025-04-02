92a66072da058610e88ebe9234747b7a22364aa0
SergeyFoma <asdfg2105c@gmail.com>
2025-04-02 10:42:41 +0500

    login logout

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,4 @@
-#<<<<<<< HEAD
-from django.urls import path
-from . import views
 
-app_name="users"
-
-urlpatterns=[
-    path("login/", views.login, name="login"),
-    path("logout/", views.logout, name="logout"),
-]
-#=======
 from django.urls import path
 from . import views
 
@@ -16,6 +6,6 @@ app_name="users"
 
 urlpatterns=[
     path("login_user/", views.login_user, name="login_user"),
-    path("logout/", views.logout, name="logout"),
+    path("logout_user/", views.logout_user, name="logout_user"),
 ]
-#>>>>>>> 1950fdc6c22b0fb305be059d47f9c2a0f234da8d
+
--- users/views.py
+++ users/views.py
@@ -1,29 +1,32 @@
-#<<<<<<< HEAD
-from django.shortcuts import render
-from django.http import HttpResponse
 
-def login(request):
-    return HttpResponse('login')
-
-def logout(request):
-    return HttpResponse('logout')
-
-# Create your views here.
-#=======
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
 from users.forms import LoginUserForm
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
 
 def login_user(request):
-    form=LoginUserForm()
+    if request.method == "POST":
+        form=LoginUserForm(request.POST)
+        if form.is_valid():
+            cd=form.cleaned_data
+            user=authenticate(request, username=cd['username'], 
+            password=cd["password"])
+            if user and user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse("women:index"))
+    else:
+        form=LoginUserForm()
     #return HttpResponse('login')
     context={
         "form":form,
     }
     return render(request, "users/login_user.html", context)
 
-def logout(request):
-    return HttpResponse('logout')
+def logout_user(request):
+    logout(request)
+    #return HttpResponse('logout')
+    return HttpResponseRedirect(reverse("users:login_user"))
 
 # Create your views here.
-#>>>>>>> 1950fdc6c22b0fb305be059d47f9c2a0f234da8d
+
