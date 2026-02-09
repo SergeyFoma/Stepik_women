bab046c7daf12e2fb67659355682ad15fee56681
SergeyFoma <asdfg2105c@gmail.com>
2026-02-09 14:09:22 +0500

    create model Women, migrate

--- women/models.py
+++ women/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Women(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    content = models.TextField(verbose_name='Текст статьи')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Дата последнего редактирования')
+    is_published = models.BooleanField(default=True, verbose_name='Опубликована')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Статья о женщине'
+        verbose_name_plural = 'Статьи о женщинах'
+        ordering = ['-time_create']  # сортируем статьи по убыванию даты создания
