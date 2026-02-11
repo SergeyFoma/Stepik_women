d0b54facd369fecfa9af4a5c7a92adb275344a9b
SergeyFoma <asdfg2105c@gmail.com>
2026-02-11 15:36:43 +0500

    update admin, views

--- women/admin.py
+++ women/admin.py
@@ -1,16 +1,10 @@
 from django.contrib import admin
 
-<<<<<<< HEAD
-from women.models import Women
-
-@admin.register(Women)
-class WomenAdmin(admin.ModelAdmin):
-    pass
-=======
 from .models import Women  # импортируем нашу модель Women
 
 # Регистрируем модель через декоратор
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
     prepopulated_fields={'slug':('title',)}
->>>>>>> new_branch
+    fields=['title', 'content', 'slug']
+
--- women/views.py
+++ women/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseNotFound
-from django.template.loader import render_to_string
+
+# from django.template.loader import render_to_string
 
 from women.models import Women
 
 
-menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+
 
 def index(request):
     # t=render_to_string('women/index.html')
     # return HttpResponse(t)
-    posts=Women.objects.all()
-    context={
-        'title':'Главная страница',
-        'menu':menu,
-        'posts':posts,
+    posts = Women.objects.filter(is_published=1)
+    context = {
+        "title": "Главная страница",
+        "menu": menu,
+        "posts": posts,
     }
     return render(request, "women/index.html", context)
 
@@ -34,19 +36,19 @@ def categories_by_slug(request, cat_slug):
     }
     return HttpResponse(f"SLUG: {context}")
 
+
 def show_post(request, post_slug):
-    pos=get_object_or_404(Women, slug=post_slug)
-    title='POST'
-    context={
-        'title':title,
-        'menu':menu,
-        "pos":pos,
-        "cat_selected":1,
+    pos = get_object_or_404(Women, slug=post_slug)
+    title = "POST"
+    context = {
+        "title": title,
+        "menu": menu,
+        "pos": pos,
+        "cat_selected": 1,
     }
     return render(request, "women/show_post.html", context)
 
 
 def page_not_found(request, exception):
-    print('exception: ', exception)
+    print("exception: ", exception)
     return HttpResponseNotFound("<h1>Page not found. Страница не существует.</h1>")
-
