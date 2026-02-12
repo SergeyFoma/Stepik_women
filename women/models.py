ff4caccfd081f924782976e099eac7d40c57a300
SergeyFoma <asdfg2105c@gmail.com>
2026-02-12 14:56:29 +0500

    ForeignKey Category

--- women/models.py
+++ women/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.urls import reverse
 
+class Category(models.Model):
+    name=models.CharField(max_length=100, db_index=True, verbose_name='Название категории')
+    slug=models.SlugField(max_length=100, db_index=True, unique=True, verbose_name='Slug')
+
+    def __str__(self):
+        return self.name
+
 class Women(models.Model):
     title = models.CharField(max_length=255, verbose_name='Заголовок')
     slug=models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL') #https://docs.djangoproject.com/en/6.0/ref/models/fields/#slugfield
@@ -8,6 +15,7 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     time_update = models.DateTimeField(auto_now=True, verbose_name='Дата последнего редактирования')
     is_published = models.BooleanField(default=True, verbose_name='Опубликована')
+    cat=models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name='Категория',blank=True,null=True)
 
     def __str__(self):
         return self.title
